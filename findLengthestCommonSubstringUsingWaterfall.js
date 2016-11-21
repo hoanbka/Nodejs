2d313977e8548012e4c681f697dfdf5b6c120f28
Albert <nguyenhoan.hut@gmail.com>
2016-11-21 15:51:41 +0700

    Using waterfall to find lengthest common substring of two given strings
    Using waterfall to find lengthest common substring of two given strings

--- findLengthestCommonSubstringUsingWaterfall.js
+++ findLengthestCommonSubstringUsingWaterfall.js
@@ -1,59 +1,59 @@
-var str1= "ahelloworld123jjhelloworld2";
-var str2="helloworld1sdfggkkhelloworld2k";
+var str1 = "ahelloworld123jjhelloworld2";
+var str2 = "helloworld1sdfggkkhelloworld2k";
 
-var subStringArr1 =[];
+var subStringArr1 = [];
 var commonSub = [];
 var async = require('async');
 
 async.waterfall([
 
-	function(callback){
+    function (callback) {
 
-		for(var i =0;i< str1.length;i++){
+        for (var i = 0; i < str1.length; i++) {
 
-			for( var j= i+1;j<= str1.length;j++){
+            for (var j = i + 1; j <= str1.length; j++) {
 
-				subStringArr1.push(str1.substring(i,j));
-			}
-		}
-		callback(null,subStringArr1);
+                subStringArr1.push(str1.substring(i, j));
+            }
+        }
+        callback(null, subStringArr1);
 
-	}, function(res1, callback){
+    }, function (res1, callback) {
 
-		// res1 =subStringArr1;
-		res1.sort(function(a,b){
-			return b.length-a.length;
-		});
+        // res1 =subStringArr1;
+        res1.sort(function (a, b) {
+            return b.length - a.length;
+        });
 
-		callback(null,res1);
+        callback(null, res1);
 
-	}, function(res2, callback){
+    }, function (res2, callback) {
 
-		//res2 =res1;
-		for(var i= 0;i< res2.length;i++){
+        //res2 =res1;
+        for (var i = 0; i < res2.length; i++) {
 
-				if(str2.includes(res2[i])){
-		
-					commonSub.push(res2[i]);
-	
-				if(res2[i].length< commonSub[0].length){
-					commonSub.pop();
-					break;
-				}
-			}
-		}
-		callback(null,commonSub);
-	}
+            if (str2.includes(res2[i])) {
+
+                commonSub.push(res2[i]);
+
+                if (res2[i].length < commonSub[0].length) {
+                    commonSub.pop();
+                    break;
+                }
+            }
+        }
+        callback(null, commonSub);
+    }
 
 
-	], function(err, results){
+], function (err, results) {
 
-	if(err){
-		return console.log(err);
-	}
+    if (err) {
+        return console.log(err);
+    }
 
-	console.log("lengthest common substring of two given string are :")
-	results.forEach(function(element){
-		console.log(element);
-	})
+    console.log("lengthest common substring of two given string are :")
+    results.forEach(function (element) {
+        console.log(element);
+    })
 })
