ee5e1e7e2a77de9e5d5181f07107ffeb9f6b5096
Albert <nguyenhoan.hut@gmail.com>
2016-11-24 09:18:35 +0700

    using reduce() to calculate sum of elements in a random array
    using reduce() to calculate sum of elements in a random array

--- reduce.js
+++ reduce.js
@@ -33,3 +33,29 @@ async.waterfall([
 
     console.log("sum = " + result);
 })
+
+//
+
+var arr2 = [5, 5, 5, 2, 2, 2, 2, 2, 9, 4];
+var counts = {};
+
+for (var i = 0; i < arr2.length; i++) {
+    var num = arr2[i];
+    counts[num] = counts[num] ? counts[num] + 1 : 1;
+}
+
+console.log(counts);
+
+//
+var myMap = new Map();
+
+for (var i = 0; i < arr2.length; i++) {
+
+    if (myMap.has(arr2[i])) {
+        myMap.set(arr2[i], myMap.get(arr2[i]) + 1);
+    } else {
+        myMap.set(arr2[i], 1);
+    }
+}
+
+console.log(myMap);
